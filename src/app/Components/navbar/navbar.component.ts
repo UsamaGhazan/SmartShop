99cc8a9a2b71c9a21153ce02f32b94ffde240d17
dawoodumar720 <dawoodumar720@gmail.com>
2024-03-07 04:42:21 -0800

    in

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { SearchBoxComponent } from '../../search-box/search-box.component';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [SearchBoxComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  
 
 }
