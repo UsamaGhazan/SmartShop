291a40044d76ca9dcf74c0f08b6fef73078da069
dawoodumar720 <dawoodumar720@gmail.com>
2024-03-08 05:06:22 -0800

    f

--- src/app/Components/product-card/product-card.component.ts
+++ src/app/Components/product-card/product-card.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReviewsComponent } from '../../reviews/reviews.component';
+import { ReviewsComponent } from '../reviews/reviews.component';
 
 @Component({
   selector: 'app-product-card',
---
+++ src/app/models/product.ts
@@ -0,0 +1,14 @@
+export interface Product{
+    id:number,
+    name:string,
+    image:string,
+    description:string,
+    brand:string,
+    category:string,
+    price:number,
+    countInStock:number,
+    rating:5,
+    numReviews:number
+    
+
+}
