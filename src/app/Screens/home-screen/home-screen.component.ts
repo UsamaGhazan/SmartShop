ef4a3b3a8b3a1a3ee29323083d13bac67653ce2f
dawoodumar720 <dawoodumar720@gmail.com>
2024-03-08 02:56:03 -0800

    A

---
+++ src/app/Screens/home-screen/home-screen.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ProductCardComponent } from '../../product-card/product-card.component';
+
+@Component({
+  selector: 'app-home-screen',
+  standalone: true,
+  imports: [ProductCardComponent],
+  templateUrl: './home-screen.component.html',
+  styleUrl: './home-screen.component.css'
+})
+export class HomeScreenComponent {
+
+}
