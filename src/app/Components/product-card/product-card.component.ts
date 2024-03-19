a44ceae3f6a78ecf9b72fb8d51290e429f10c6eb
dawoodumar720 <dawoodumar720@gmail.com>
2024-03-19 00:58:37 -0700

    a

--- src/app/Components/product-card/product-card.component.ts
+++ src/app/Components/product-card/product-card.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReviewsComponent } from '../reviews/reviews.component';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [ReviewsComponent],
+  imports: [ReviewsComponent,CommonModule,AsyncPipe],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
+  http=inject(HttpClient)
+  products$=this.http.get('https://fakestoreapi.com/products') as Observable<any[]>
   rating!:number
   imgSrc!:string
   
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,9 @@ import { routes } from './app.routes';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { counterReducer } from './states/counter/counter.reducer';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore(), provideEffects(),
+  providers: [provideRouter(routes), provideStore(), provideEffects(),provideHttpClient(),
   provideState({name:'counter',reducer:counterReducer})]
 };
