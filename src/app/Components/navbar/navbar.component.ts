33cf9e0f78526940391ded38a933d16a3cfe612d
dawoodumar720 <dawoodumar720@gmail.com>
2024-03-21 23:25:35 -0700

    f

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SearchBoxComponent } from '../search-box/search-box.component';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [SearchBoxComponent],
+  imports: [SearchBoxComponent,RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeScreenComponent } from './Screens/home-screen/home-screen.component';
 import { CounterComponent } from './counter/counter.component';
+import { CartComponent } from './Components/cart-component/cart-component.component';
 
 export const routes: Routes = [{
     path:'',component:HomeScreenComponent,
 },
 {
     path:'counter',component:CounterComponent
+
+
+},
+{
+    path:'cart',component:CartComponent
 
 }];
